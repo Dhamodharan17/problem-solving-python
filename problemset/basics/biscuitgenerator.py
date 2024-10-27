69905a7721052d0f06a423051c735fdef27b885d
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-10-27 19:22:41 +0530

    Create biscuitgenerator.py

---
+++ problemset/basics/biscuitgenerator.py
@@ -0,0 +1,37 @@
+# AP 1
+def biscuitGenerator():
+    
+    #Get input
+    a,b,t = input().split()
+    a = float(a)
+    b = float(b)
+    t = float(t)
+    totalBiscuits = 0
+    i =1
+    limit = a
+    
+    # Generate biscuit until given time t
+    while limit <= t+0.5:
+        totalBiscuits = totalBiscuits + b
+        i = i+1 #subsequent
+        # next time where machine will generate b biscuits
+        limit = a*i
+        
+    print(int(totalBiscuits))
+    
+biscuitGenerator()
+
+# AP2
+
+def biscuitGenerator():
+    
+    a,b,t = map(float, input().split())
+    
+    totalSlots = (t+0.5) // a
+    
+    total_biscuits =totalSlots*b
+    
+    print(int(total_biscuits))
+    
+biscuitGenerator()
+
