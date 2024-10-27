2238dfa1ddc54183f1cfeca8484adb9a5bdf3a12
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-10-27 18:32:58 +0530

    Update basics.py

--- problemset/basics.py
+++ problemset/basics.py
@@ -4,3 +4,4 @@ https://vjudge.net/problem/AtCoder-abc169_a
 https://vjudge.net/problem/CodeForces-630A
 https://vjudge.net/problem/AtCoder-abc209_a
 https://vjudge.net/problem/AtCoder-abc124_a
+https://vjudge.net/problem/AtCoder-abc226_a
