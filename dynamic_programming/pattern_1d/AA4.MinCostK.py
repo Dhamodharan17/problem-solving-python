bd652fca6bec2d0d861815bfc4eb8faa52b833e9
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-11-10 21:24:07 +0530

    Update AA4.MinCostK.py

--- dynamic_programming/pattern_1d/AA4.MinCostK.py
+++ dynamic_programming/pattern_1d/AA4.MinCostK.py
@@ -46,3 +46,26 @@ class Solution:
             return  dp[n]
             
         return minimizeUtil(arr, k, n-1, dp)
+
+#Bottom UP
+class Solution:
+    def minimizeCost(self, k, arr):
+        
+        length = len(arr)
+        dp = [float('inf')] * length
+        dp[0] = 0 # energy req. to reach 0
+        
+        for n in range(1,length):
+            globalMincost = float('inf')
+            # Find min cost jump at current n
+            for i in range(1,k+1):
+                jump = n-i
+                if jump >= 0:
+                   current_jump = abs(arr[n] - arr[jump])
+                   #Take remaining from dp array
+                   #In top down , we call function for remaning
+                   globalMincost = min(globalMincost,current_jump + dp[jump] )
+            dp[n] = globalMincost
+            
+        return  dp[length-1]
+            
