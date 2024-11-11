3bda668ea9d94d3366ee7003ed162bf6fb5380fe
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-11-11 13:23:56 +0530

    Update AA4.HouseRobber1.py

--- dynamic_programming/pattern_1d/AA4.HouseRobber1.py
+++ dynamic_programming/pattern_1d/AA4.HouseRobber1.py
@@ -46,3 +46,28 @@ class Solution(object):
             dp[i] = max(rob, skip)
 
         return dp[N-1]
+# Space opt
+class Solution(object):
+    # larger problem : max amount can be robbed at all houses
+    def rob(self, nums):
+
+        N = len(nums)
+        if N == 1:
+            return nums[0]
+        # Since we just need 2 houses at back
+        # Either to chose or not
+        prev2 = nums[0]
+        # max can be robbed at house
+        prev1 = max(nums[1],nums[0])
+
+        for i in range(2, N):
+            rob = nums[i] + prev2
+            skip = prev1
+            cur = max(rob, skip)
+            prev2 = prev1
+            prev1 = cur
+
+        return prev1
+
+
+        
