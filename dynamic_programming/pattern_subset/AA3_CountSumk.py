44a7ca60b9bbb03215da5bdd5b794f5b434f416c
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-11-16 23:19:49 +0530

    Update AA3_CountSumk.py

--- dynamic_programming/pattern_subset/AA3_CountSumk.py
+++ dynamic_programming/pattern_subset/AA3_CountSumk.py
@@ -1,24 +1,64 @@
 #recursion
 class Solution:
 	def perfectSum(self, arr, n, sum):
-		# code here
 		
-		def util(current_index, current_target):
-		    
-		    if current_target == 0:
+		def util(i, target):
+		    #smallest problem - when sum = 0 any set can make it by empty set
+		    if target == 0 :
 		        return 1
-	        
-	        if current_index < 0:
-	            return 0
-	        
-	        take = 0
-	        if arr[current_index] <= current_target:
-	            take = util(current_index-1, current_target-arr[current_index])
-	       
-	        skip = util(current_index-1, current_target)
-	       
-	        return skip + take
-	       
-	    return util(n-1, sum)
+		    
+		    if i < 0 :
+		        return 0
+		    
+		    pick = 0
+		    if arr[i] <= target :
+		        pick = util(i-1, target - arr[i])
+            
+            skip = util(i-1, target)
+            
+            return pick + skip
+        
+        return util(n-1, sum)
 
-#topdown
+#bottom up
+class Solution:
+	def perfectSum(self, arr, n, sum):
+	   MOD = 10**9 + 7  # Modulo value
+	   dp = [[0 for _ in range(sum+1)] for _ in range(n+1)]
+	    
+	   # all subsets can make sum 0
+	   for i in range(n+1):
+	        dp[i][0] = 1
+	   
+	   for i in range(1,n+1):
+	       # start from sum 0, since sum element can be 0
+	       for cur_sum in range(0,sum+1):
+	            pick = 0
+	            if arr[i-1] <= cur_sum:
+	                pick = dp[i-1][cur_sum - arr[i-1]] % MOD
+	            skip = dp[i-1][cur_sum] % MOD
+	            dp[i][cur_sum] = (pick + skip) % MOD
+	            
+	   return dp[n][sum]
+
+#space
+class Solution:
+	def perfectSum(self, arr, n, sum):
+	    
+	   MOD = 10**9 + 7 
+	   
+	   prev = [0 for _ in range(sum+1)]
+	   # since all subsets can make 0
+	   prev[0] = 1
+	   
+	   for i in range(1,n+1):
+	       current = [0 for _ in range(sum+1)]
+	       current[0] = 1
+	       for cur_sum in range(0,sum+1):
+	            pick = 0
+	            if arr[i-1] <= cur_sum:
+	                pick = prev[cur_sum - arr[i-1]] % MOD
+	            skip = prev[cur_sum] % MOD
+	            current[cur_sum] = (pick + skip) % MOD
+	       prev = current
+	   return prev[sum]
