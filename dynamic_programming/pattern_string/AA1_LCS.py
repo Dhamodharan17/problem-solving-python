8913b3a202753fa65c1348e327bba8fb14cf2b45
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-11-19 20:43:25 +0530

    Update AA1_LCS.py

--- dynamic_programming/pattern_string/AA1_LCS.py
+++ dynamic_programming/pattern_string/AA1_LCS.py
@@ -40,6 +40,34 @@ class Solution(object):
         
         return dp[n1][n2]
 
+#space
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        
+        n1 = len(text1)
+        n2 = len(text2)
+
+        prev = [0 for _ in range(n2+1)] 
+        current = [0 for _ in range(n2+1)] 
+
+        # Lcs with empty string
+        prev[0] = 0
+        current[0] = 0
+        
+        for ind1 in range(1,n1+1):
+            for ind2 in range(1,n2+1):
+                if text1[ind1-1] == text2[ind2-1]:
+                    #dp[ind1-1][ind2-1] - both string reduced by 1
+                    current[ind2] = 1 + prev[ind2-1]
+                else:
+                    current[ind2] = max(prev[ind2],current[ind2-1])
+            prev = current
+        
+        return prev[n2]
+
+       
+
+        
        
 
         
