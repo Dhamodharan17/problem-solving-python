e285d710c5f7c8804e75805923c83547d8ee8f22
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-11-20 20:16:31 +0530

    Update AA1_LIS.py

--- dynamic_programming/pattern_lis/AA1_LIS.py
+++ dynamic_programming/pattern_lis/AA1_LIS.py
@@ -18,4 +18,37 @@ class Solution(object):
         
         return util(n-1,-1)
 
+
+#top down
+#smaller problem -> find lis where previous = prev
+class Solution(object):
+    def lengthOfLIS(self, nums):
         
+        n = len(nums)
+        dp = [[-1 for _ in range(n+1)] for _ in range(n)]
+
+        def util(prev, current):
+
+            if current == len(nums):
+                return 0
+            
+            if dp[current][prev+1] != -1:
+                return dp[current][prev+1]
+
+            pick = 0
+            if prev == -1 or nums[current] > nums[prev]:
+                # prev will change since we are continuing seq
+                pick = 1 + util(current, current+1)
+
+            skip = util(prev, current+1)
+
+            #both moves next element, prev matters
+            dp[current][prev+1] = max(pick, skip)
+            return dp[current][prev+1]
+        
+        return util(-1, 0)
+
+
+
+
+
