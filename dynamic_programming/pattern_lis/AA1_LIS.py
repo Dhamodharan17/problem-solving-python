25b3df469945fcb8235247107a612433a8c6d5e3
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-11-20 20:44:16 +0530

    Update AA1_LIS.py

--- dynamic_programming/pattern_lis/AA1_LIS.py
+++ dynamic_programming/pattern_lis/AA1_LIS.py
@@ -20,7 +20,10 @@ class Solution(object):
 
 
 #top down
-#smaller problem -> find lis where previous = prev
+#bigger problem - find lis of set, length n
+#smaller problem ->  find lis of set, length n-1
+#when we are reducing the problem size, prev will change
+#also we need to try all possible ways
 class Solution(object):
     def lengthOfLIS(self, nums):
         
@@ -48,7 +51,32 @@ class Solution(object):
         
         return util(-1, 0)
 
+#bottom up
+class Solution(object):
+    def lengthOfLIS(self, nums):
 
+        n = len(nums)
+        dp =[ [0 for _ in range(n+1)] for _ in range(n+1)]
+
+        # LIS for empty array
+        for i in range(n+1):
+            dp[n][i] = 0
+
+        for cur in range(n-1, -1, -1):
+            for prev in range(cur-1,-2,-1):
+
+                not_take = dp[cur+1][prev+1]#prev since -1 , +1
+
+                take = 0
+                if prev == -1 or nums[cur] > nums[prev]:
+                    take = 1 + dp[cur+1][cur+1]#cur
+
+                dp[cur][prev+1] = max(take, not_take)
+
+        return dp[0][0]
+
+       
+        
 
 
 
