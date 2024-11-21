68cec3365cf7c44ab7d6fdf806c017ac69e991dc
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-11-21 20:35:52 +0530

    Update AA2_Min_Cost_Cut_Stick.py

--- dynamic_programming/pattern_mcm/AA2_Min_Cost_Cut_Stick.py
+++ dynamic_programming/pattern_mcm/AA2_Min_Cost_Cut_Stick.py
@@ -20,7 +20,32 @@ class Solution(object):
         #start from last array index
         return util(1,len(cuts)-2)
         
+#top down
+    
+    class Solution(object):
+    def minCost(self, n, cuts):
+        
+        cuts = [0] + cuts + [n]
+        cuts.sort()
+        c = len(cuts)-2
+
+        dp = [[0 for _ in range(c+2)] for _ in range(c+2)]
+
+        for i in range(c,0,-1):
+            for j in range(1,c+1):
+                if i > j:
+                    continue
+                mini = float('inf')
+                for k in range(i, j+1):
+                    current_cost = cuts[j+1] - cuts[i-1]+ dp[i][k-1] + dp[k+1][j]
+                    mini = min(current_cost, mini)
+                dp[i][j] = mini
+
+        return dp[1][c]
+
+
 
     
+        
         
         
