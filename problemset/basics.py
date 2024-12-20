7e65cb95cf0be1d7b763a61dacffa534a1ee50a8
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-12-20 09:45:04 +0800

    Delete problemset directory

--- problemset/basics.py
+++
@@ -1,8 +0,0 @@
-#Basic Problems
-https://vjudge.net/problem/Aizu-ITP1_1_A
-https://vjudge.net/problem/AtCoder-abc169_a
-https://vjudge.net/problem/CodeForces-630A
-https://vjudge.net/problem/AtCoder-abc209_a
-https://vjudge.net/problem/AtCoder-abc124_a
-https://vjudge.net/problem/AtCoder-abc226_a
-https://vjudge.net/problem/AtCoder-abc222_a
