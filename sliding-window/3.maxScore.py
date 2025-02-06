232ef8bd41badb4044507b80e874e4aab9b2478a
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2025-02-06 09:25:57 +0530

    Update 3.maxScore.py

--- sliding-window/3.maxScore.py
+++ sliding-window/3.maxScore.py
@@ -23,4 +23,30 @@ class Solution:
 
         return max(ans, bsum)
 
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+
+        ans, n = 0, len(cardPoints)
+
+        prefix_sum = [0] * n
+        prefix_sum[0] = cardPoints[0]
+        for i in range(1,n):
+            prefix_sum[i] = prefix_sum[i-1] + cardPoints[i]
+
+        suffix_sum = [0] * n
+        suffix_sum[n-1] = cardPoints[n-1]
+        for i in range(n-2,-1,-1):
+            suffix_sum[i] = suffix_sum[i+1] + cardPoints[i]
+
+            
+        for i in range(k):
+            fsum = prefix_sum[i]
+            print(i)
+            bsum = 0 if i== k-1 else suffix_sum[n-k+i+1]
+            print(n-k+i)
+            ans = max(ans, fsum+bsum)
+        
+        ans = max(ans,suffix_sum[n-k])
+        return ans
+
         
